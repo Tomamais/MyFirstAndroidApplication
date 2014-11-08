958d7a4b7ee0c6b2e61c70096967dcfc91bdf5fe
Tomamaisgit config --global user.name Tomamaisgit config --global user.name Tomamais <tomamais@gmail.com>
2014-11-08 21:36:08 -0200

    Initial commit

---
+++ app/src/androidTest/java/tomasvasquez/com/br/myfirstandroidapplication/ApplicationTest.java
@@ -0,0 +1,13 @@
+package tomasvasquez.com.br.myfirstandroidapplication;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
